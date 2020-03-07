fac4a47ec3422738c6a231a4879639087c82f53c
A <gokalp@mail.com>
2020-03-07 18:47:07 +1100

    Fix the visualisation notebook

--- lib/jupyter_io.py
+++ lib/jupyter_io.py
@@ -27,11 +27,11 @@ def read_results():
     latest_result_file = max(result_files, key=os.path.getctime)
     print('reading results from', latest_result_file)
     return pd.read_csv( latest_result_file,
-                        index_col=[0] )
+                        index_col=[0], parse_dates=['predicted_at'] )
 
 def read_predictions():
-    return pd.read_csv( f'{BASE_DIR}/predictions_on_test/predictions.csv.xz',
-                        index_col=[0, 1], parse_dates=True )
+    return pd.read_csv( f'{BASE_DIR}/output/predictions_on_test.csv.xz',
+                        index_col=[0, 1], parse_dates=['date'] )
 
 def read_ticker_price_data(ticker, from_date):
     data = pd.read_sql_query(   f"""
@@ -39,8 +39,7 @@ def read_ticker_price_data(ticker, from_date):
                                 WHERE date >= '{from_date}'  
                                 ORDER BY date ASC
                                 """,
-                                f'sqlite:////var/stox/stox.db',
-                                index_col=['date'] )
-    data.index = pd.to_datetime(data.index)
+                                f'sqlite:///{BASE_DIR}/db/stox.db',
+                                index_col=['date'], parse_dates=['date'] )
     data = data.resample('D').pad()
     return data
