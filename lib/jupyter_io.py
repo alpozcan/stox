f7c2fa2d450d554f0b1c607e289ee7e8598381e0
A <gokalp@mail.com>
2020-03-08 18:10:52 +1100

    Also include the last prediction on visualise notebook's graphs

--- lib/jupyter_io.py
+++ lib/jupyter_io.py
@@ -22,10 +22,11 @@ import os, glob
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
 
-def read_results():
+def read_results(silent=False):
     result_files = glob.glob(f'{BASE_DIR}results/*.csv')
     latest_result_file = max(result_files, key=os.path.getctime)
-    print('reading results from', latest_result_file)
+    if not silent:
+        print('reading results from', latest_result_file)
     return pd.read_csv( latest_result_file,
                         index_col=[0], parse_dates=['predicted_at'] )
 
@@ -33,9 +34,18 @@ def read_predictions():
     return pd.read_csv( f'{BASE_DIR}/output/predictions_on_test.csv.xz',
                         index_col=[0, 1], parse_dates=['date'] )
 
+def read_all_predictions():
+    """ Combine the last prediction with predictions on test data """
+    predictions = read_predictions() # has predictions on test data
+    for ticker, result in read_results(silent=True).iterrows():
+        predictions.loc[(result.predicted_at, ticker), 'prediction'] = result.prediction
+        
+    return predictions
+
 def read_ticker_price_data(ticker, from_date):
     data = pd.read_sql_query(   f"""
-                                SELECT `date`, `open`, `close` FROM `{ticker}` 
+                                SELECT `date`, `open`, `close`, ((`open`+`close`) / 2) AS `mid_price` 
+                                FROM `{ticker}` 
                                 WHERE date >= '{from_date}'  
                                 ORDER BY date ASC
                                 """,
