50b1b6f462ca9c6200b57e43daeadf8ed45bc608
Alp <gokalp@mail.com>
2020-04-13 15:44:28 +1000

    write results and predictions on test into sql server database

--- lib/db.py
+++ lib/db.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # DB wrappers
 
-import pyodbc
+import pyodbc, sqlalchemy, urllib
+
+CONN_STRING = """
+DRIVER={ODBC Driver 17 for SQL Server};
+SERVER=host;
+DATABASE=stox;
+UID=stox;
+PWD=stox;
+"""
 
 def db_connection():
-    return pyodbc.connect   (   'DRIVER={ODBC Driver 17 for SQL Server};'
-                                'SERVER=host;'
-                                'DATABASE=stox;'
-                                'UID=stox;'
-                                'PWD=stox;')
+    return pyodbc.connect(CONN_STRING)
+
+def db_engine():
+    return sqlalchemy.create_engine(f"mssql+pyodbc:///?odbc_connect={urllib.parse.quote_plus(CONN_STRING)}", fast_executemany=True)
