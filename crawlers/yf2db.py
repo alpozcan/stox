9056cadf84af2b2da3f7da1dcfa5ad4ca16fddc5
Alp <gokalp@mail.com>
2020-04-18 15:32:03 +1000

    make yf2db much smarter

--- crawlers/yf2db.py
+++ crawlers/yf2db.py
@@ -35,7 +35,26 @@ cursor = conn.cursor()
 def getCount():
     cursor.execute(f"SELECT COUNT(*) FROM {TABLE}")
     return int(cursor.fetchone()[0])
-count = getCount() # initial row count before the inserts
+
+def RowExists(key):
+    cursor.execute(f""" SELECT COUNT(*) FROM {TABLE}
+                        WHERE   date   = '{key[0]}'
+                            AND market = '{key[1]}'
+                            AND ticker = '{key[2]}'
+                    """)
+    return True if int(cursor.fetchone()[0]) == 1 else False
+
+def getRow(key):
+    cursor.execute(f""" SELECT * FROM {TABLE}
+                        WHERE   date   = '{key[0]}'
+                            AND market = '{key[1]}'
+                            AND ticker = '{key[2]}'
+                    """)
+    result = cursor.fetchone()
+    return [ str(result[0]).replace('-','') ] + [ str(c) for c in result[1:3] ] + [ (float(c) if c is not None else None) for c in result[3:] ]
+
+initial_count = getCount() # initial row count before the inserts
+num_inserted, num_updated, num_skipped = 0, 0, 0
 
 for i in INDICES:
     if i['market'] not in MARKETS:
@@ -46,32 +65,52 @@ for i in INDICES:
     print('Will fetch', len(tickers), 'tickers for', i['index_ticker'])
 
     for ticker in tickers:
-        # print('\nfetch', ticker)
+        if VERBOSE:
+            print('----------', 'fetching', ticker, '----------')
+
         yf_ticker = yf.Ticker(ticker)
         data = yf_ticker.history(period="max", auto_adjust=False).reset_index()
 
         for day_data in data.iterrows():
             columns = [ None if type(c) == float and isnan(c) else c for c in day_data[1].values ]
 
-            query = f'INSERT INTO {TABLE}([date], [market], [ticker], [open], [high], [low], [close], [volume], [dividend], [split])'
-            query +=' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+            new_values = [
+                columns[0].strftime("%Y%m%d"), # date
+                i['market'],
+                ticker,
+                columns[1], # open
+                columns[2], # high
+                columns[3], # low
+                columns[4], # close
+                # columns[5], # adj close
+                columns[6], # volume
+                columns[7], # dividends
+                columns[8] # splits
+            ]
 
-            if VERBOSE:
-                print('insert', i['market'], ticker, columns)
+            if RowExists(new_values[:3]):
+                old_values = getRow(new_values[:3])
+                if (new_values == old_values):
+                    if VERBOSE:
+                        print('SKIP', new_values, sep='\t')
+                    num_skipped += 1
+                else:
+                    if VERBOSE:
+                        print('UPDATE', new_values, sep='\t')
+                        print('   old:', old_values)
 
-            cursor.execute  (   query,
-                                columns[0].strftime("%Y%m%d"), # date
-                                i['market'],
-                                ticker,
-                                columns[1], # open
-                                columns[2], # high
-                                columns[3], # low
-                                columns[4], # close
-                                # columns[5], # adj close
-                                columns[6], # volume
-                                columns[7], # dividends
-                                columns[8] # splits
-                            )
+                    query =f""" UPDATE {TABLE} SET [open] = ?, [high] = ?, [low] = ?, [close] = ?, [volume] = ?, [dividend] = ?, [split] = ?
+                                WHERE   [date]   = ? AND [market] = ? AND [ticker] = ? """
+                    cursor.execute(query, *new_values[3:], *new_values[:3])
+                    num_updated += 1
+            else:
+                if VERBOSE:
+                    print('INSERT', new_values, sep='\t')
+
+                query =f""" INSERT INTO {TABLE}([date], [market], [ticker], [open], [high], [low], [close], [volume], [dividend], [split])
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) """
+                cursor.execute(query, *new_values)
+                num_inserted += 1
 
         if INFO:
             info = yf_ticker.info
@@ -79,4 +118,5 @@ for i in INDICES:
 
         conn.commit()
 
-print('yf2db.py:', getCount() - count, 'rows inserted.')
+print('yf2db.py:', num_inserted, 'rows inserted,', num_updated, 'rows updated,', num_skipped, 'skipped.')
+assert (getCount() - initial_count) == num_inserted
